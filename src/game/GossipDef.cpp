2d7ffe85c680704c3eb978bb468b711d36c179dd
NoFantasy <nofantasy@nf.no>
2010-02-07 03:44:21 +0100

    [a184]Apply correct formula for counting quest XP reward

--- src/game/GossipDef.cpp
+++ src/game/GossipDef.cpp
@@ -512,7 +512,12 @@ void PlayerMenu::SendQuestGiverQuestDetails( Quest const *pQuest, uint64 npcGUID
                 data << uint32(0);
         }
 
-        data << uint32(pQuest->GetRewOrReqMoney());
+        // send rewMoneyMaxLevel explicit for max player level, else send RewOrReqMoney
+        if (pSession->GetPlayer()->getLevel() >= sWorld.getConfig(CONFIG_MAX_PLAYER_LEVEL))
+            data << uint32(pQuest->GetRewMoneyMaxLevel());
+        else
+            data << uint32(pQuest->GetRewOrReqMoney());
+
         data << uint32(pQuest->XPValue(pSession->GetPlayer()));
     }
 
@@ -768,7 +773,11 @@ void PlayerMenu::SendQuestGiverOfferReward( Quest const* pQuest, uint64 npcGUID,
             data << uint32(0);
     }
 
-    data << uint32(pQuest->GetRewOrReqMoney());             // money
+    // send rewMoneyMaxLevel explicit for max player level, else send RewOrReqMoney
+    if (pSession->GetPlayer()->getLevel() >= sWorld.getConfig(CONFIG_MAX_PLAYER_LEVEL))
+        data << uint32(pQuest->GetRewMoneyMaxLevel());
+    else
+        data << uint32(pQuest->GetRewOrReqMoney());
     data << uint32(pQuest->XPValue(pSession->GetPlayer())); // xp
 
     // rewarded honor points. Multiply with 10 to satisfy client
