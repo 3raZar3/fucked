9ef85663ec8d4d66e3c007c4b330dbe133d805e2
przemratajczak <przemratajczak@wp.pl>
2010-03-08 18:58:23 +0100

    [a270]Small adjustment to instance crashfix

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -2488,11 +2488,11 @@ bool InstanceMap::Add(Player *player)
                     // set up a solo bind or continue using it
                     if(!playerBind)
                         player->BindToInstance(mapSave, false);
-                    else
+                    else if (playerBind->save != mapSave)
                     {
                         // cannot jump to a different instance without resetting it
                         // lets send him to nearest graveyard or homebind
-                        if (WorldSafeLocsEntry const *Grave = sObjectMgr.GetClosestGraveYard(player->GetPositionX(), player->GetPositionY(), player->GetPositionZ(), player->GetMapId(), player->GetTeam() ))
+                        if (WorldSafeLocsEntry const *ClosestGrave = sObjectMgr.GetClosestGraveYard(player->GetPositionX(), player->GetPositionY(), player->GetPositionZ(), player->GetMapId(), player->GetTeam() ))
                             player->RepopAtGraveyard();
                         else player->RelocateToHomebind();
                         error_log("Player %s (GUID %u) logged into instance which is not bound to. Cheat, exploit?",player->GetName(),player->GetGUID());
