ceb9c94e1c801f9185eb918a6e79bd65cee87058
Ceris <Ceris@getmangops.com>
2010-03-25 01:08:22 +0100

    [a314] implement aura 219 (call stabled pet)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -2148,6 +2148,10 @@ Creature* Player::GetNPCIfCanInteractWith(ObjectGuid guid, uint32 npcflagmask)
     if (guid.IsEmpty() || !IsInWorld() || isInFlight())
         return NULL;
 
+    //needed for call stabled pet
+    if (GetGUID() == guid.GetRawValue())
+        return ((Creature*)this);
+
     // exist (we need look pets also for some interaction (quest/etc)
     Creature *unit = GetMap()->GetCreatureOrPetOrVehicle(guid);
     if (!unit)
