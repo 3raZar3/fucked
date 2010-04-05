085f0f435d390a49bd3d0131c4facf2d7579b1df
przemratajczak <przemratajczak@wp.pl>
2010-04-05 21:40:57 +0200

    [a334] Minor SQL cleanups

--- addition/129_mangos_gossip_menu_option.sql
+++ addition/129_mangos_gossip_menu_option.sql
@@ -1,2 +1,3 @@
+DELETE FROM `gossip_menu_option` WHERE id = 16;
 INSERT INTO `gossip_menu_option` VALUES 
 (0, 16, 0, 'Purchase a Dual Talent Specialization.', 18, 16, 0, 0, 0, 0, 10000000, 'Are you sure you wish to purchase a Dual Talent Specialization?', 0, 0, 0, 0, 0, 0, 0, 0, 0);
