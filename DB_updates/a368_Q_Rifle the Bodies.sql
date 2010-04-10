e11ee7ea0dbc0226a633ab60395d110f28a2265f
koksneo <koksneo@github.com>
2010-04-10 22:50:54 +0200

    [a369] quest Planning for the Future

--- DB_updates/a368_Q_Rifle the Bodies.sql
+++ DB_updates/a368_Q_Rifle the Bodies.sql
@@ -1,5 +1,5 @@
 -- Support for quests (11999,12000) (Rifle the Bodies A and H)
-DELETE FROM npc_spellclick_spells WHERE npc_entry IN(26200,26477);
+DELETE FROM npc_spellclick_spells WHERE npc_entry = 26477;
 INSERT INTO npc_spellclick_spells VALUES
 (26477,47096,12000,1,12000,3),
 (26477,61832,12000,1,12000,1),
