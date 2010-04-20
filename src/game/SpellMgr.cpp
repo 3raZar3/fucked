d05db2e83dd15a50325a00629e24dc921fa15923
przemratajczak <przemratajczak@wp.pl>
2010-04-21 01:27:15 +0200

    [a401] Implement stacking rules with LW crafted drums

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1364,6 +1364,22 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
     if ((spellInfo_1->Attributes & SPELL_ATTR_PASSIVE)!=(spellInfo_2->Attributes & SPELL_ATTR_PASSIVE))
         return false;
 
+    // My rules! :D
+    if (spellInfo_1->AttributesEx6 & SPELL_ATTR_EX6_UNK26 && spellInfo_2->AttributesEx6 & SPELL_ATTR_EX6_UNK26)
+    {
+        // Marks and Gifts of the Wild
+        if (spellInfo_1->EffectApplyAuraName[2] == 143 && spellInfo_2->EffectApplyAuraName[2] == 143)
+            return true;
+
+        // Blessings of Kings and Blessing of Forgotten Kings
+        if (spellInfo_1->EffectApplyAuraName[0] == 137 && spellInfo_1->EffectApplyAuraName[0] == 137)
+            return true;
+
+        // Battle Shout and Blessings of Might
+        if (spellInfo_1->EffectApplyAuraName[1] == 124 && spellInfo_1->EffectApplyAuraName[1] == 124)
+            return true;
+    }
+
     // Specific spell family spells
     switch(spellInfo_1->SpellFamilyName)
     {
