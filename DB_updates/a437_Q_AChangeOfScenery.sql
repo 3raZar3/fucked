6076c49cb4fef616c8d895b23a17dbdeea3f1f3c
przemratajczak <przemratajczak@wp.pl>
2010-05-04 01:03:17 +0200

    [a443] Fixed auras rewarded in quest The Crone`s Bargain, also restored propper faction templates

--- DB_updates/a437_Q_AChangeOfScenery.sql
+++
@@ -1,4 +0,0 @@
--- Quest :: A Change of Scenery - 12921
--- wrong factions for alliance nad horde
-UPDATE `creature_template` SET `faction_A` = '35',
-`faction_H` = '35' WHERE `entry` = 29975 LIMIT 1 ;
