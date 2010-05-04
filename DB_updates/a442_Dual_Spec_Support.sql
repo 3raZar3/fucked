88bf3e9d82a45e9f7b0636b2ba6668d11f594de8
Grandres <grundil@wp.pl>
2010-05-04 23:18:06 +0200

    [a445] Missing gossip_menu option in class trainers. (Provided by me and Koksneo). Dual Talent Specialization Achievement (provided by Steve_o). The gossip_menu text isn't propely for the most of trainers. Need source for this.

--- DB_updates/a442_Dual_Spec_Support.sql
+++
@@ -1,7 +0,0 @@
--- Dual Spec - fixed typos
-UPDATE gossip_menu_option SET option_id= 1, action_script_id=6 WHERE menu_id=0 AND id=16;
-
--- Dual Talent Specialization Achievement 
-DELETE FROM achievement_criteria_requirement WHERE criteria_id=9619;
-INSERT INTO achievement_criteria_requirement VALUES 
-(9619,0,63624,0);
