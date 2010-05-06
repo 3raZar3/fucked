160c14a97da2ddc5b529073af1047dcb2dfed45a
Grandres <grundil@wp.pl>
2010-05-06 20:59:43 +0200

    Text in [a448] is correct now. Thx Czopor and Tank

--- DB_updates/a448_Finish_me.sql
+++ DB_updates/a448_Finish_me.sql
@@ -1,5 +1,4 @@
 -- Support for quest Finish me! (13232)
-# Note! 'Rest in peace...' is temporary text. Need source.
 
 -- Dying Soldier gossip
 UPDATE creature_template SET gossip_menu_id=31304 WHERE entry=31304;
@@ -11,7 +10,7 @@ INSERT INTO gossip_menu (entry,text_id,cond_1,cond_1_val_1,cond_1_val_2) VALUES
 DELETE FROM gossip_menu_option WHERE menu_id=31304;
 INSERT INTO gossip_menu_option (menu_id,id,option_icon,option_text,option_id,npc_option_npcflag,action_menu_id,action_script_id,cond_1,cond_1_val_1) VALUES
 (31304,0,0,'GOSSIP_OPTION_QUESTGIVER',2,2,0,0,0,0),
-(31304,1,0,'Rest in peace...',1,1,0,31341,9,13232);				
+(31304,1,0,'Travel well, hero of the Alliance!',1,1,0,31341,9,13232);				
 
 DELETE FROM gossip_scripts WHERE id=31341;
 INSERT INTO gossip_scripts (id,delay,command,datalong,datalong2) VALUES
