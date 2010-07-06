a364c0edb614fb6335c8b7828409b5c2eb7c7c2b
GMHAZE <iceis2@hotmail.com>
2010-07-05 21:34:41 -0400

    reset

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "b9702152c6a3cb4cc47692559f1fb163f9f7e075"
+ #define REVISION_ID "*"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
