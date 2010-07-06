ed1943e98e8ae3ef0a991ff834e1315c2a47b2df
Dark <dark_rulerz@hotmail.com>
2010-07-06 14:53:01 +0200

    GO Master Loot (from Wowka, slightly edited...hope this helps to get rid of our looting problems)

--- src/game/Creature.h
+++ src/game/Creature.h
@@ -33,7 +33,6 @@
 struct SpellEntry;
 
 class CreatureAI;
-class Group;
 class Quest;
 class Player;
 class WorldSession;
@@ -612,8 +611,6 @@ class MANGOS_DLL_SPEC Creature : public Unit
         float GetRespawnRadius() const { return m_respawnradius; }
         void SetRespawnRadius(float dist) { m_respawnradius = dist; }
 
-        void StartGroupLoot(Group* group, uint32 timer);
-
         void SendZoneUnderAttackMessage(Player* attacker);
 
         void SetInCombatWithZone();
@@ -681,10 +678,6 @@ class MANGOS_DLL_SPEC Creature : public Unit
         
         uint32 m_unPlayerDamageDone;
         uint32 m_unUnitDamageDone;
-
-        uint32 m_groupLootTimer;                            // (msecs)timer used for group loot
-        uint32 m_groupLootId;                               // used to find group which is looting corpse
-        void StopGroupLoot();
 
         // vendor items
         VendorItemCounts m_vendorItemCounts;
